b1397958f2e01cfc562c4ea71fb2e69ecd0b7dc3
Lorenzo542 <agostinetto.lorenzo2002@gmail.com>
2025-04-22 16:20:43 +0200

    added video

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
 const addBtn = document.getElementById('add-plant');
 const plantDiv = document.getElementById('plant-list');
-const photoDiv = document.getElementById('photo-div')
-const videoEl = document.getElementById('my-video')
-let stream = null
+const photoDiv = document.getElementById('photo-div');
+const videoEl = document.getElementById('my-video');
+let stream = null;
+
 
 addBtn.addEventListener('click', function() {
-    
     if (photoDiv.classList.value === 'hide'){
-        
         plantDiv.classList.add('hide');
         photoDiv.classList.remove('hide');
-        getMedia(e)
+        getMedia();
     } else {
-
         plantDiv.classList.remove('hide');
         photoDiv.classList.add('hide');
+        stopMedia();
     }
 });
 
 async function getMedia() {
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({
+        stream = await navigator.mediaDevices.getUserMedia({
             audio: false, 
             video: true
         });
-        
+        videoEl.srcObject = stream;
+        videoEl.play(); 
     } catch (error) {
         console.log('User denied camera access permission');
-        return null;
-    }
 
-    videoEl.srcObject = stream;
+        photoDiv.innerHTML = '<p>Camera access denied. Please allow camera access.</p>';
+    }
+}
+function stopMedia() {
+    if (stream) {
+        stream.getTracks().forEach(track => track.stop());
+        stream = null;
+        videoEl.srcObject = null;
+    }
 }
