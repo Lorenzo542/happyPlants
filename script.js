fb98296e7c5839c22e3156c1c8c48806cdef071c
Lorenzo542 <agostinetto.lorenzo2002@gmail.com>
2025-04-22 16:17:16 +0200

    added video

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const addBtn = document.getElementById('add-plant');
 const plantDiv = document.getElementById('plant-list');
 const photoDiv = document.getElementById('photo-div')
+const videoEl = document.getElementById('my-video')
+let stream = null
+
 addBtn.addEventListener('click', function() {
     
     if (photoDiv.classList.value === 'hide'){
         
         plantDiv.classList.add('hide');
         photoDiv.classList.remove('hide');
-        navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
-            console.log('Got MediaStream:', stream);
-        })
-        .catch(error => {
-            console.error('Error accessing media devices.', error);
-        });
+        getMedia(e)
     } else {
 
         plantDiv.classList.remove('hide');
         photoDiv.classList.add('hide');
     }
 });
+
+async function getMedia() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: false, 
+            video: true
+        });
+        
+    } catch (error) {
+        console.log('User denied camera access permission');
+        return null;
+    }
+
+    videoEl.srcObject = stream;
+}
